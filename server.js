d0358a0f260a5772beb0021345696840281a9603
Only-Abhiram <abhi1@AbhiChikki.(none)>
2025-12-28 10:13:35 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,118 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const crypto = require('crypto');
+const  {getInstagramLongLivedToken}  = require('./token.js');
+
+
+const app = express();
+
+// IMPORTANT: raw body needed for signature verification
+app.use(bodyParser.json({
+  verify: (req, res, buf) => {
+    req.rawBody = buf;
+  }
+}));
+
+const PORT = process.env.PORT || 5000;
+const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
+const APP_SECRET = process.env.APP_SECRET;
+
+
+/**
+ * Utility: Verify Meta X-Hub-Signature
+ */
+function isValidSignature(req) {
+  const signature = req.headers['x-hub-signature'];
+  if (!signature || !APP_SECRET) return false;
+
+  const [algo, hash] = signature.split('=');
+
+  const expectedHash = crypto
+    .createHmac(algo, APP_SECRET)
+    .update(req.rawBody)
+    .digest('hex');
+
+  return hash === expectedHash;
+}
+
+/**
+ * Phase A: Webhook Verification (GET)
+ */
+app.get('/instagram', (req, res) => {
+  const mode = req.query['hub.mode'];
+  const token = req.query['hub.verify_token'];
+  const challenge = req.query['hub.challenge'];
+
+  if ( token === VERIFY_TOKEN) {
+    console.log('Webhook verified successfully');
+    return res.status(200).send(challenge);
+  }
+
+  console.log('Webhook verification failed');
+  return res.sendStatus(403);
+});
+
+/**
+ * Phase B: Event Delivery (POST)
+ */
+app.post('/instagram', (req, res) => {
+  // Optional but recommended
+//   if (!isValidSignature(req)) {
+//     console.warn('Invalid X-Hub-Signature');
+//     return res.sendStatus(401);
+//   }
+
+  console.log('Webhook event received');
+  console.dir(req.body, { depth: null });
+
+  /**
+   * Typical structure:
+   * req.body.entry[].changes[].value
+   */
+
+  // Always respond fast
+  res.sendStatus(200);
+
+  // Process asynchronously later if needed
+});
+
+// Redirect Auth
+
+app.get('/auth/callback', async (req, res) => {
+  const { code, state } = req.query;
+
+  if (!code) {
+    return res.status(400).send('Missing auth code');
+  }
+
+  try {
+  
+
+    const { instagramUserId, accessToken , expiresIn } = await getInstagramLongLivedToken(code);
+    console.log('Instagram User ID: ', instagramUserId);
+    console.log('Access Token(Long Lived): ', accessToken);
+    console.log('Expires In: ', expiresIn);
+
+    console.log('Instagram connected successfully ✅');
+    // put them in the database
+    // TODO: Redirect to dashboard
+    res.redirect('https://www.google.com')
+    // res.redirect('/success.html');
+
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('OAuth failed');
+  }
+});
+
+/**
+ * Health check
+ */
+app.get('/', (req, res) => {
+  res.send('Instagram webhook is running');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
---
+++ token.js
@@ -0,0 +1,71 @@
+require('dotenv').config();
+const axios = require('axios');
+const qs = require('querystring');
+
+/**
+ * Exchange Instagram auth code for long-lived access token
+ */
+ async function getInstagramLongLivedToken(authCode) {
+  try {
+    // ----------------------------------
+    // STEP 1: auth code -> short-lived token
+    // ----------------------------------
+    console.log({
+        client_id: process.env.INSTAGRAM_APP_ID,
+        client_secret: process.env.INSTAGRAM_APP_SECRET,
+        grant_type: 'authorization_code',
+        redirect_uri: process.env.INSTAGRAM_REDIRECT_URI,
+        code: authCode
+      })
+    const shortTokenResponse = await axios.post(
+      'https://api.instagram.com/oauth/access_token',
+      qs.stringify({
+        client_id: process.env.INSTAGRAM_APP_ID,
+        client_secret: process.env.INSTAGRAM_APP_SECRET,
+        grant_type: 'authorization_code',
+        redirect_uri: "https://cd8b144ef946.ngrok-free.app/auth/callback",
+        code: authCode
+      }),
+      {
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+        }
+      }
+    );
+
+    const shortTokenData = shortTokenResponse.data.data[0];
+
+    const shortLivedToken = shortTokenData.access_token;
+    const instagramUserId = shortTokenData.user_id;
+    console.log('Short Lived Token: ', shortLivedToken);
+    // ----------------------------------
+    // STEP 2: short-lived -> long-lived token
+    // ----------------------------------
+    const longTokenResponse = await axios.get(
+      'https://graph.instagram.com/access_token',
+      {
+        params: {
+          grant_type: 'ig_exchange_token',
+          client_secret: process.env.INSTAGRAM_APP_SECRET,
+          access_token: shortLivedToken
+        }
+      }
+    );
+
+    return {
+      instagramUserId: instagramUserId,
+      accessToken: longTokenResponse.data.access_token,
+      expiresIn: longTokenResponse.data.expires_in
+    };
+
+  } catch (error) {
+    if (error.response) {
+      throw new Error(
+        `Instagram OAuth error: ${JSON.stringify(error.response.data)}`
+      );
+    }
+    throw error;
+  }
+}
+
+module.exports = { getInstagramLongLivedToken };
